8fa4cabc6693dbc309b872efc37605845f24e5ae
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-01 22:15:48 +0300

    2

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
+    "os"
+
     "github.com/go-chi/chi/v5"
     "github.com/go-chi/chi/v5/middleware"
     "github.com/wolf4b12/metrics-sv.git/internal/storage"
@@ -10,6 +13,17 @@ import (
 )
 
 func main() {
+    // Определение флага для адреса сервера
+    addr := flag.String("a", "localhost:8080", "адрес эндпоинта HTTP-сервера")
+
+    // Парсинг флагов
+    flag.Parse()
+
+    // Проверка наличия неизвестных флагов
+    if flag.NArg() > 0 {
+        log.Fatalf("Неизвестный флаг: %s\n", os.Args[flag.NArg()-1])
+    }
+
     storage := storage.NewMemStorage()
 
     // Создание нового роутера с использованием chi
@@ -18,20 +32,16 @@ func main() {
     // Настройка middleware для журналирования запросов
     router.Use(middleware.Logger)
 
-    // Маршрут для обновления метрик
+    // Маршруты для обработки запросов
     router.Post("/update/{metricType}/{metricName}/{metricValue}", handler.UpdateHandler(storage))
-
-    // Маршрут для получения конкретной метрики
     router.Get("/value/{metricType}/{metricName}", handler.ValueHandler(storage))
-
-    // Маршрут для получения списка всех метрик
     router.Get("/", handler.ListMetricsHandler(storage))
 
     server := &http.Server{
-        Addr:    "localhost:8080",
+        Addr:    *addr,
         Handler: router,
     }
 
-    log.Printf("Starting server on http://localhost:8080\n")
+    log.Printf("Запуск сервера на http://%s\n", *addr)
     log.Fatal(server.ListenAndServe())
 }
