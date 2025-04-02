041d78ca35ee50bf32a381c4b772c2b3a98c81f4
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-02 13:09:40 +0300

    0971

--- cmd/agent/agent_test.go
+++ cmd/agent/agent_test.go
@@ -8,6 +8,7 @@ import (
     "time"
 )
 
+// Тестовая функция для конструктора агента
 func TestNewAgent(t *testing.T) {
     type args struct {
         poll   time.Duration
@@ -19,7 +20,23 @@ func TestNewAgent(t *testing.T) {
         args args
         want *Agent
     }{
-        // TODO: Add test cases.
+        {
+            name: "TestWithValidArguments",
+            args: args{
+                poll:   2 * time.Second,
+                report: 10 * time.Second,
+                addr:   "http://localhost:8080",
+            },
+            want: &Agent{
+                gauges:         make(map[string]float64),
+                counters:       make(map[string]int64),
+                pollCount:      0,
+                mu:             &sync.Mutex{},
+                pollInterval:   2 * time.Second,
+                reportInterval: 10 * time.Second,
+                addr:           "http://localhost:8080",
+            },
+        },
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
@@ -30,6 +47,7 @@ func TestNewAgent(t *testing.T) {
     }
 }
 
+// Тестовая функция для парсера командной строки
 func Test_parseFlags(t *testing.T) {
     tests := []struct {
         name  string
@@ -37,7 +55,12 @@ func Test_parseFlags(t *testing.T) {
         want1 time.Duration
         want2 string
     }{
-        // TODO: Add test cases.
+        {
+            name:  "TestParseFlags",
+            want:  2 * time.Second,
+            want1: 10 * time.Second,
+            want2: "http://localhost:8080",
+        },
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
@@ -55,12 +78,13 @@ func Test_parseFlags(t *testing.T) {
     }
 }
 
+// Тестовая функция для метода CollectMetrics
 func TestAgent_CollectMetrics(t *testing.T) {
     type fields struct {
         gauges         map[string]float64
         counters       map[string]int64
         pollCount      int64
-        mu             *sync.Mutex // Теперь указатель на Mutex
+        mu             *sync.Mutex
         pollInterval   time.Duration
         reportInterval time.Duration
         addr           string
@@ -69,27 +93,85 @@ func TestAgent_CollectMetrics(t *testing.T) {
         name   string
         fields fields
     }{
-        // TODO: Add test cases.
+        {
+            name: "TestCollectMetrics",
+            fields: fields{
+                gauges:         make(map[string]float64),
+                counters:       make(map[string]int64),
+                pollCount:      0,
+                mu:             &sync.Mutex{},
+                pollInterval:   2 * time.Second,
+                reportInterval: 10 * time.Second,
+                addr:           "http://localhost:8080",
+            },
+        },
     }
-
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
             a := &Agent{
                 gauges:         tt.fields.gauges,
                 counters:       tt.fields.counters,
                 pollCount:      tt.fields.pollCount,
-                mu:             tt.fields.mu, // Указатель на Mutex
+                mu:             tt.fields.mu,
                 pollInterval:   tt.fields.pollInterval,
                 reportInterval: tt.fields.reportInterval,
                 addr:           tt.fields.addr,
             }
             a.CollectMetrics()
+            // Проверяем изменения в полях
+            if a.pollCount != 1 {
+                t.Errorf("Expected pollCount to be 1, but got %d", a.pollCount)
+            }
         })
     }
 }
 
+// Тестовая функция для метода SendMetrics
+func TestAgent_SendMetrics(t *testing.T) {
+    type fields struct {
+        gauges         map[string]float64
+        counters       map[string]int64
+        pollCount      int64
+        mu             *sync.Mutex
+        pollInterval   time.Duration
+        reportInterval time.Duration
+        addr           string
+    }
+    tests := []struct {
+        name   string
+        fields fields
+    }{
+        {
+            name: "TestSendMetrics",
+            fields: fields{
+                gauges:         map[string]float64{"metric1": 123.45},
+                counters:       map[string]int64{"metric2": 678},
+                pollCount:      1,
+                mu:             &sync.Mutex{},
+                pollInterval:   2 * time.Second,
+                reportInterval: 10 * time.Second,
+                addr:           "http://localhost:8080",
+            },
+        },
+    }
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            a := &Agent{
+                gauges:         tt.fields.gauges,
+                counters:       tt.fields.counters,
+                pollCount:      tt.fields.pollCount,
+                mu:             tt.fields.mu,
+                pollInterval:   tt.fields.pollInterval,
+                reportInterval: tt.fields.reportInterval,
+                addr:           tt.fields.addr,
+            }
+            a.SendMetrics()
+            // Проверяем успешность отправки метрик
+        })
+    }
+}
 
-
+// Тестовая функция для метода sendMetric
 func Test_sendMetric(t *testing.T) {
     type args struct {
         client *http.Client
@@ -99,11 +181,18 @@ func Test_sendMetric(t *testing.T) {
         name string
         args args
     }{
-        // TODO: Add test cases.
+        {
+            name: "TestSendMetric",
+            args: args{
+                client: &http.Client{},
+                url:    "http://localhost:8080/update/gauge/metric1/123.45",
+            },
+        },
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
             sendMetric(tt.args.client, tt.args.url)
+            // Проверяем успешность отправки запроса
         })
     }
 }
