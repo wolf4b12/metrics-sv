572a60ac1027520b54d13b5a6f98d8b47534bafb
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-07 16:04:02 +0300

    delete //commit  TODO: Add test cases

--- internal/agent/parseflags/parseflags_test.go
+++ internal/agent/parseflags/parseflags_test.go
@@ -12,7 +12,7 @@ func TestParseFlags(t *testing.T) {
 		want1 time.Duration
 		want2 string
 	}{
-		// TODO: Add test cases.
+
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
