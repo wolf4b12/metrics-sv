4d29d98ece73f5816fa44b61fe712e08aa87cf7d
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-09 01:20:48 +0300

    0904

--- internal/agent/agentmethods/agentmethods.go
+++ internal/agent/agentmethods/agentmethods.go
@@ -3,14 +3,12 @@ package agentmethods
 import (
     "fmt"
     "log"
-    "math/rand"
     "net/http"
     "runtime"
     "sync"
     "time"
+    metrics "github.com/wolf4b12/metrics-sv.git/internal/agent/metrics"
 )
-
-
 
 type Agent struct {
     gauges         map[string]float64
@@ -21,8 +19,6 @@ type Agent struct {
     reportInterval time.Duration
     addr           string
 }
-
-
 
 func NewAgent(poll, report time.Duration, addr string) *Agent {
     return &Agent{
@@ -35,8 +31,6 @@ func NewAgent(poll, report time.Duration, addr string) *Agent {
     }
 }
 
-
-
 func (a *Agent) CollectMetrics() {
     var memStats runtime.MemStats
 
@@ -45,39 +39,22 @@ func (a *Agent) CollectMetrics() {
 
         a.mu.Lock()
 
-        // Runtime gauge metrics
-        a.gauges["Alloc"] = float64(memStats.Alloc)
-        a.gauges["BuckHashSys"] = float64(memStats.BuckHashSys)
-        a.gauges["Frees"] = float64(memStats.Frees)
-        a.gauges["GCCPUFraction"] = memStats.GCCPUFraction
-        a.gauges["GCSys"] = float64(memStats.GCSys)
-        a.gauges["HeapAlloc"] = float64(memStats.HeapAlloc)
-        a.gauges["HeapIdle"] = float64(memStats.HeapIdle)
-        a.gauges["HeapInuse"] = float64(memStats.HeapInuse)
-        a.gauges["HeapObjects"] = float64(memStats.HeapObjects)
-        a.gauges["HeapReleased"] = float64(memStats.HeapReleased)
-        a.gauges["HeapSys"] = float64(memStats.HeapSys)
-        a.gauges["LastGC"] = float64(memStats.LastGC)
-        a.gauges["Lookups"] = float64(memStats.Lookups)
-        a.gauges["MCacheInuse"] = float64(memStats.MCacheInuse)
-        a.gauges["MCacheSys"] = float64(memStats.MCacheSys)
-        a.gauges["MSpanInuse"] = float64(memStats.MSpanInuse)
-        a.gauges["MSpanSys"] = float64(memStats.MSpanSys)
-        a.gauges["Mallocs"] = float64(memStats.Mallocs)
-        a.gauges["NextGC"] = float64(memStats.NextGC)
-        a.gauges["NumForcedGC"] = float64(memStats.NumForcedGC)
-        a.gauges["NumGC"] = float64(memStats.NumGC)
-        a.gauges["OtherSys"] = float64(memStats.OtherSys)
-        a.gauges["PauseTotalNs"] = float64(memStats.PauseTotalNs)
-        a.gauges["StackInuse"] = float64(memStats.StackInuse)
-        a.gauges["StackSys"] = float64(memStats.StackSys)
-        a.gauges["Sys"] = float64(memStats.Sys)
-        a.gauges["TotalAlloc"] = float64(memStats.TotalAlloc)
+        // Используем карту для инициализации всех runtime-метрик
+        runtimeMetrics := metrics.GetRuntimeMetricsGauge(memStats)
+        for key, value := range runtimeMetrics {
+            a.gauges[key] = value
+        }
 
-        // Custom metrics
-        a.gauges["RandomValue"] = rand.Float64()
+        // Используем карту для инициализации кастомных метрик
+        customMetrics := metrics.GetCustomMetrics()
+        for key, value := range customMetrics {
+            a.counters[key] = value
+        }
+
+        // Обновляем счетчики
         a.pollCount++
         a.counters["PollCount"] = a.pollCount
+
         a.mu.Unlock()
         time.Sleep(a.pollInterval)
     }
---
+++ internal/agent/metrics/metrics.go
@@ -0,0 +1,48 @@
+package metrics
+
+import (
+
+"runtime"
+
+ "math/rand"
+
+)
+// GetRuntimeMetricsMap возвращает карту с метриками runtime
+func GetRuntimeMetricsGauge(memStats runtime.MemStats) map[string]float64 {
+    return map[string]float64{
+        "Alloc":                  float64(memStats.Alloc),
+        "BuckHashSys":            float64(memStats.BuckHashSys),
+        "Frees":                  float64(memStats.Frees),
+        "GCCPUFraction":          float64(memStats.GCCPUFraction),
+        "GCSys":                  float64(memStats.GCSys),
+        "HeapAlloc":              float64(memStats.HeapAlloc),
+        "HeapIdle":               float64(memStats.HeapIdle),
+        "HeapInuse":              float64(memStats.HeapInuse),
+        "HeapObjects":            float64(memStats.HeapObjects),
+        "HeapReleased":           float64(memStats.HeapReleased),
+        "HeapSys":                float64(memStats.HeapSys),
+        "LastGC":                 float64(memStats.LastGC),
+        "Lookups":                float64(memStats.Lookups),
+        "MCacheInuse":            float64(memStats.MCacheInuse),
+        "MCacheSys":              float64(memStats.MCacheSys),
+        "MSpanInuse":             float64(memStats.MSpanInuse),
+        "MSpanSys":               float64(memStats.MSpanSys),
+        "Mallocs":                float64(memStats.Mallocs),
+        "NextGC":                 float64(memStats.NextGC),
+        "NumForcedGC":            float64(memStats.NumForcedGC),
+        "NumGC":                  float64(memStats.NumGC),
+        "OtherSys":               float64(memStats.OtherSys),
+        "PauseTotalNs":           float64(memStats.PauseTotalNs),
+        "StackInuse":             float64(memStats.StackInuse),
+        "StackSys":               float64(memStats.StackSys),
+        "Sys":                    float64(memStats.Sys),
+        "TotalAlloc":             float64(memStats.TotalAlloc),
+    }
+}
+
+
+func GetCustomMetrics() map[string]int64 {
+    return map[string]int64{
+        "RandomValue": int64(rand.Intn(1000000)), // Кастомная метрика RandomValue
+    }
+}
