9c65cb3000632e0a2c6e97bf0daa68b12a331359
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-23 09:30:28 +0300

    add file save metrics + main

--- cmd/server/server_main.go
+++ cmd/server/server_main.go
@@ -2,23 +2,23 @@ package main
 
 import (
     "log"
+    "time"
+
     server   "github.com/wolf4b12/metrics-sv.git/internal/server/srv"
     config   "github.com/wolf4b12/metrics-sv.git/internal/server/config"
 )
 
-
-
 func main() {
-
     cfg, err := config.NewConfig()
-
     if err != nil {
         log.Fatalf("ошибка при создании конфигурации: %v", err)
     }
 
-    
-    
-    srv := server.NewServer(cfg.GetAddr()) // Используем метод GetAddr()
+    // Настройки загрузки и интервала сохранения
+    loadPrevious := true           // Нужно ли подгружать старые метрики при старте
+    interval := 5 * time.Minute    // Интервал сохранения метрик (каждый 5 минут)
+
+    srv := server.NewServer(cfg.GetAddr(), loadPrevious, interval)
 
     err = srv.Run()
     if err != nil {
