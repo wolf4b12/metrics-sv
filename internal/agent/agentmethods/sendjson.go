a7ccb4e6d0740cbf3c10e5267a7809749bde3657
snkhokhlov <snkhokhlov@sberbank.ru>
2025-04-27 12:52:05 +0300

    decomp send json

--- internal/agent/agentmethods/sendjson.go
+++ internal/agent/agentmethods/sendjson.go
@@ -1,111 +1,79 @@
 package agentmethods
 
 import (
-    "encoding/json"
+    "bytes"
+//    "encoding/json"
     "fmt"
     "log"
     "net/http"
     "time"
-	"bytes"
+
+    metrics "github.com/wolf4b12/metrics-sv.git/internal/agent/metricsagent"
 )
+
+
+// sendMetric отправляет одну метрику в виде POST-запроса
+func (a *Agent) sendMetric(metric metrics.Metrics) {
+    data, err := metric.MarshalJSON()
+    if err != nil {
+        a.handleErrorAndContinue("маршализации метрики в JSON", err)
+        return
+    }
+
+    // Формируем URL для отправки метрики
+    url := fmt.Sprintf("http://%s/update/", a.addr)
+
+    // Сжимаем данные
+    compressedData, err := a.compressPayload(data)
+    if err != nil {
+        a.handleErrorAndContinue("сжатия метрики", err)
+        return
+    }
+
+    // Формируем запрос с Gzip-данными
+    req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(compressedData))
+    if err != nil {
+        a.handleErrorAndContinue("формирования запроса", err)
+        return
+    }
+
+    // Устанавливаем заголовки
+    a.SetHeaders(req, "application/json")
+
+    // Выполняем запрос
+    resp, err := a.client.Do(req)
+    if err != nil {
+        a.handleErrorAndContinue("отправки метрики", err)
+        return
+    }
+
+    // Обрабатываем ответ
+    if err := a.handleResponse(resp); err != nil {
+        a.handleErrorAndContinue("обработки ответа", err)
+    }
+}
 
 // SendJSONCollectedMetrics отправляет собранные метрики в формате JSON
 func (a *Agent) SendJSONCollectedMetrics() {
     for {
         a.mu.Lock()
 
-        // Отправляем каждую метрику отдельно
+        // Отправляем Gauges
         for _, gauge := range a.Gauges {
             if gauge.Value == nil {
                 log.Printf("Отсутствует обязательный параметр 'Value' для сенсора '%s'\n", gauge.ID)
                 continue
             }
-
-            // Маршализируем единичную метрику в JSON
-            data, err := json.Marshal(gauge)
-            if err != nil {
-                a.handleErrorAndContinue("маршализации метрики в JSON", err)
-                continue
-            }
-
-            // Формируем URL для отправки метрики
-            url := fmt.Sprintf("http://%s/update/", a.addr)
-
-            // Сжимаем данные
-            compressedData, err := a.compressPayload(data)
-            if err != nil {
-                a.handleErrorAndContinue("сжатия метрики", err)
-                continue
-            }
-
-            // Формируем запрос с Gzip-данными
-            req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(compressedData))
-            if err != nil {
-                a.handleErrorAndContinue("формирования запроса", err)
-                continue
-            }
-
-            // Устанавливаем заголовки
-            a.SetHeaders(req, "application/json")
-
-            // Выполняем запрос
-            resp, err := a.client.Do(req)
-            if err != nil {
-                a.handleErrorAndContinue("отправки метрики", err)
-                continue
-            }
-
-            // Обрабатываем ответ
-            if err := a.handleResponse(resp); err != nil {
-                a.handleErrorAndContinue("обработки ответа", err)
-            }
+            a.sendMetric(gauge)
         }
 
-        // Повторяем аналогичную процедуру для счетчиков
+        // Отправляем Counters
         for _, counter := range a.Counters {
             if counter.Delta == nil {
                 log.Printf("Отсутствует обязательный параметр 'Delta' для счетчика '%s'\n", counter.ID)
                 continue
             }
-
-            // Маршализируем единичную метрику в JSON
-            data, err := json.Marshal(counter)
-            if err != nil {
-                a.handleErrorAndContinue("маршализации метрики в JSON", err)
-                continue
-            }
-
-            // Формируем URL для отправки метрики
-            url := fmt.Sprintf("http://%s/update/", a.addr)
-
-            // Сжимаем данные
-            compressedData, err := a.compressPayload(data)
-            if err != nil {
-                a.handleErrorAndContinue("сжатия метрики", err)
-                continue
-            }
-
-            // Формируем запрос с Gzip-данными
-            req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(compressedData))
-            if err != nil {
-                a.handleErrorAndContinue("формирования запроса", err)
-                continue
-            }
-
-            // Устанавливаем заголовки
-            a.SetHeaders(req, "application/json")
-
-            // Выполняем запрос
-            resp, err := a.client.Do(req)
-            if err != nil {
-                a.handleErrorAndContinue("отправки метрики", err)
-                continue
-            }
-
-            // Обрабатываем ответ
-            if err := a.handleResponse(resp); err != nil {
-                a.handleErrorAndContinue("обработки ответа", err)
-            }
+            a.sendMetric(counter)
         }
 
         a.mu.Unlock()
--- internal/agent/metricsagent/metricsstructjson.go
+++ internal/agent/metricsagent/metricsstructjson.go
@@ -1,9 +1,32 @@
 package metricsag
 
+import (
+
+ "encoding/json"
+
+)
+
+
+
 // Метрика для отправки на сервер
 type Metrics struct {
     ID    string   `json:"id"`              // имя метрики
     MType string   `json:"type"`            // тип метрики (gauge или counter)
     Delta *int64   `json:"delta,omitempty"` // изменение значения (для счётчиков)
     Value *float64 `json:"value,omitempty"` // текущее значение (для датчиков)
+}
+
+
+type Metric interface {
+    ID() string
+    MarshalJSON() ([]byte, error)
+}
+
+
+// Метод интерфейса для идентификации метрики
+func (m *Metrics) GetID() string { return m.ID }
+
+// Маршаллизация метрики в JSON
+func (m *Metrics) MarshalJSON() ([]byte, error) {
+    return json.Marshal(m)
 }
